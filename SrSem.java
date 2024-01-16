54f4d96e7f707bd266ff29f2fd36f67d0faefe34
Adi Duggal <duggaadi25@7hills.org>
2024-01-16 12:54:25 -0500

    f

--- SrSem.java
+++ SrSem.java
@@ -1,19 +1,24 @@
 import java.io.File;
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileNotFoundException;
 
 public class SrSem {
-    try {
-		File info = new File("SrSeminar_RawData.csv");
-		Scanner fileBot = new Scanner(info);
-		while (fileBot.hasNextLine()) {
-			String data = fileBot.nextLine();
-		}
-		fileBot.close();
-    }
-    catch (FileNotFoundException e) {
-        System.out.println("there was an error. try to fix it or something idk.");
-        e.printStackTrace();
+
+
+
+    public void read() {
+        try {
+		    File info = new File("SrSeminar_RawData.csv");
+		    Scanner fileBot = new Scanner(info);
+		    while (fileBot.hasNextLine()) {
+		    	String data = fileBot.nextLine();
+		    }
+		    fileBot.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("there was an error. try to fix it or something idk.");
+            e.printStackTrace();
+        }
     }
         
 }

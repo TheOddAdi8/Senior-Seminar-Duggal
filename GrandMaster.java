128a3a5e20f2e4a34fc5b7ad8676f011eb2cacaf
Adi Duggal <duggaadi25@7hills.org>
2024-01-20 15:32:07 -0500

    made minimal progress

--- GrandMaster.java
+++ GrandMaster.java
@@ -1,5 +1,6 @@
 public class GrandMaster { //tester
     public static void main(String[] args) {
         SrSem s1 = new SrSem();
+        s1.read();
     }
 }
--- SrSem.java
+++ SrSem.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public class SrSem {
 
-
+    public ArrayList<String[]> list = new ArrayList<String[]>();
 
     public void read() {
         try {
@@ -12,13 +12,18 @@ public class SrSem {
 		    Scanner fileBot = new Scanner(info);
 		    while (fileBot.hasNextLine()) {
 		    	String data = fileBot.nextLine();
-		    }
+                list.add(data.split(","));
+            }
 		    fileBot.close();
         }
         catch (FileNotFoundException e) {
             System.out.println("there was an error. try to fix it or something idk.");
             e.printStackTrace();
         }
+
+        System.out.println(list.get(0)[0]);
     }
+
+    
         
 }
